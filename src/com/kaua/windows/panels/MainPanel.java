a39657477da69b3c59b5f8ca683c2dcfefa1a4c4
Kaua <kauacalixtocontato@gmail.com>
2021-09-15 21:43:48 -0300

    added 2 screens and some methods

--- src/com/kaua/windows/panels/MainPanel.java
+++ src/com/kaua/windows/panels/MainPanel.java
@@ -1,5 +1,9 @@
 package com.kaua.windows.panels;
 
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -7,9 +11,11 @@ import javax.swing.JPanel;
 public class MainPanel extends JPanel {
 
 	private JLabel fundo, person;
-
+	private int load = 0;
+	
 	public MainPanel() {
 		init();
+		new Temporizador().start();
 	}
 
 	public void init() {
@@ -23,5 +29,33 @@ public class MainPanel extends JPanel {
 		fundo.setBounds(0, 0, 677, 260);
 		add(fundo);
 	}
+	
+	public class Temporizador extends Thread{
+        public void run(){
+            while( load <100){
+                try {
+                	sleep(50);
+                    load +=1;
+                    System.out.println(load);
+                    // Quando chegar 100%, esta função chamarar outra tela.	
+        			if(load >= 100) {
+        				setVisible(false);
+        				new InicialScreen().setVisible(true);
+        			}
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedAudioFileException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (LineUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        }
+    }
 
 }
---
+++ src/com/kaua/windows/panels/PrologueScreen.java
@@ -0,0 +1,11 @@
+package com.kaua.windows.panels;
+
+import javax.swing.JPanel;
+
+public class PrologueScreen extends JPanel{
+
+	public PrologueScreen() {
+		
+	}
+	
+}
